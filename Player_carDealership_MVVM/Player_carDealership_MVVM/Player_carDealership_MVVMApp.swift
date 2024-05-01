d7b545c22f7a42a598a1391ec0e9d6969d7fde31
Yevgeny Ly <globchenko16@gmail.com>
2024-05-01 14:23:08 +0300

    - app init

---
+++ Player_carDealership_MVVM/Player_carDealership_MVVM/Player_carDealership_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Player_carDealership_MVVMApp.swift
+//  Player_carDealership_MVVM
+//
+//  Created by Евгений Л on 01.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Player_carDealership_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
